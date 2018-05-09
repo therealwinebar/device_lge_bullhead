ae50848c585d5cd7582ac7d19ff01f08436692e2
Anirudh Gupta <anirudhgupta109@gmail.com>
2018-05-09 11:01:44 +0000

    Update Bullhead fingerprint to OPM4.171019.016.A1/4720815
    Signed-off-by: Anirudh Gupta <anirudhgupta109@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -35,6 +35,6 @@ PRODUCT_MANUFACTURER := LGE
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM4.171019.016.A1 4720815 release-keys"
 
-BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM2.171019.029/4657601:user/release-keys
+BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM4.171019.016.A1/4720815:user/release-keys
